8a2ff9d56d63603d949bd50fb192f9759556bebc
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-05-24 23:41:54 +0200

    first commit

---
+++ src/main/java/com/marketplace_ep/marketplace_EP/MarketplaceEpApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace_ep.marketplace_EP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceEpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceEpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketplace_ep/marketplace_EP/MarketplaceEpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace_ep.marketplace_EP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceEpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
